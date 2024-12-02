9afcdeab5a0e78f5fb982947d7a4c90f490f4b84
Wollacy <carvalho_1990@outlook.com>
2024-12-02 13:40:37 -0300

    Usando Props - Projeto Final

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src: livro},
+    {nome: 'Cangaceiro JavaScript: Uma aventura nosertão da programação.', id: 2, src: livro},
+    {nome: 'Apache Kafka e Spring Boot', id: 3, src: livro},
+]
